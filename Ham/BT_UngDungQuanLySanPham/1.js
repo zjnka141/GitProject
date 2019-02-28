3a344b577206ab5f088eb2e23649a85e4b889009
zjnka141 <zjnka141@gmail.com>
2019-02-28 11:10:10 +0700

    Update to improve UX

--- Ham/BT_UngDungQuanLySanPham/1.js
+++ Ham/BT_UngDungQuanLySanPham/1.js
@@ -3,7 +3,7 @@ var app = new function () {
         'Xiaomi Redmi Note 4', 'Apple iPhone 6S', 'Apple iPhone 8 Plus', 'Oppo A71'];
     this.add = function () {
         var element = document.getElementById("add-name");
-        if (element.value) {
+        if (element.value && !this.products.includes(element.value)) {
             this.products.push(element.value);
         }
         this.show();
@@ -25,16 +25,16 @@ var app = new function () {
         this.show();
     }
     this.edit = function (index) {
-        var data='';
+        var data = '';
         for (var i = 0; i < this.products.length; i++) {
             if (i == index) {
                 data += '<tr>'
-                data += '<td style="width:60%;height: 60px;"><input autofocus type="text" value="' 
-                + this.products[i] + '" id="editTxt" /></td>';
+                data += '<td style="width:60%;height: 60px;"><input autofocus type="text" value="'
+                    + this.products[i] + '" id="editTxt" /></td>';
                 data += '<td><input type="button" onclick="app.save(' + i + ')" value="Save"></td>';
                 data += '<td><input type="button" onclick="app.show()" value="X"></td>';
                 data += '</tr>';
-            }else{
+            } else {
                 data += '<tr>'
                 data += '<td style="width:60%;height: 60px;">' + this.products[i] + '</td>';
                 data += '<td><input type="button" onclick="app.edit(' + i + ')" value="Edit"></td>';
@@ -43,10 +43,30 @@ var app = new function () {
             }
         }
         document.getElementById("products").innerHTML = data;
-        document.getElementById("editTxt").select();
+        var editElement = document.getElementById("editTxt");
+        editElement.focus();
+        // To move cusor to the end of textbox
+        var value = editElement.value;
+        editElement.value = '';
+        editElement.value = value;
+        // End
     }
-    this.save = function (index){
-        this.products[index]=document.getElementById("editTxt").value;
+    this.save = function (index) {
+        this.products[index] = document.getElementById("editTxt").value;
         this.show();
     }
+}
+function eventPress() {
+    window.addEventListener("keydown", function (event) {
+        if (document.getElementById("editTxt")) {
+            switch (event.keyCode) {
+                case 27:
+                    document.getElementById("editTxt").parentElement.nextSibling.nextSibling.children[0].click();
+                    break;
+                case 13:
+                    document.getElementById("editTxt").parentElement.nextSibling.children[0].click();
+                    break;
+            }
+        }
+    });
 }
